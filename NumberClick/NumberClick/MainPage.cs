912b48e1fab3b8310ac8ff064c49009099d56056
Ilya Petrash <ilya.petrash@inbox.ru>
2016-07-04 00:48:37 +0600

    Add project files.

---
+++ NumberClick/NumberClick/MainPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NumberClick
+{
+    public class MainPage : ContentPage
+    {
+        int clicksNumber = 0;
+        Label label = new Label() { HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center };
+        Button button = new Button() { Text = "Кликай!" };
+
+        public MainPage()
+        {
+            updateStates();
+
+            button.Clicked += (object sender, EventArgs e) =>
+            {
+                clicksNumber++;
+                updateStates();
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    button,
+                    label,
+                }
+            };
+        }
+
+        private void updateStates()
+        {
+            label.Text = String.Format("Всего кликов {0}.", clicksNumber);
+        }
+    }
+}
