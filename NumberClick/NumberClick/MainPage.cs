bf863d5fb90778d8e762f80b80c9b09e2a25a6ac
Ilya Petrash <ilya.petrash@inbox.ru>
2016-07-04 23:16:22 +0600

    Небольшие доработки

--- NumberClick/NumberClick/MainPage.cs
+++ NumberClick/NumberClick/MainPage.cs
@@ -6,38 +6,64 @@ using System.Text;
 
 using Xamarin.Forms;
 
-namespace NumberClick
-{
-    public class MainPage : ContentPage
-    {
+namespace NumberClick {
+    public class MainPage : ContentPage {
+        int maxNumberClick = 100;
         int clicksNumber = 0;
+
+        Label infoLabel = new Label() { HorizontalTextAlignment = TextAlignment.Center };
         Label label = new Label() { HorizontalTextAlignment = TextAlignment.Center };
         Button button = new Button() { Text = "Кликай!" };
+        Button button2 = new Button() { Text = "Кликай!" };
+        Button button3 = new Button() { Text = "Кликай!" };
 
-        public MainPage()
-        {
+        public MainPage() {
             updateStates();
 
-            button.Clicked += (object sender, EventArgs e) =>
-            {
-                clicksNumber++;
-                updateStates();
-            };
+            infoLabel.Text = String.Format("Привет!\nЦель этого великолепного приложения "
+                + "прокачать твои навыки нажатия на кнопки.\n"
+                + "Всего нужно кликнуть {0} раз.", maxNumberClick);
 
-            Content = new StackLayout
-            {
+            button.Clicked += clicked;
+
+            button2.IsVisible = false;
+            button2.Clicked += clicked;
+
+            button3.IsVisible = false;
+            button3.Clicked += clicked;
+
+            Content = new StackLayout {
                 VerticalOptions = LayoutOptions.Center,
 
                 Children = {
+                    infoLabel,
                     button,
+                    button2,
+                    button3,
                     label,
                 }
             };
         }
 
-        private void updateStates()
-        {
-            label.Text = String.Format("Всего кликов {0}.", clicksNumber);
+        private void updateStates() {
+            if (clicksNumber == 10) {
+                infoLabel.Text = "Ты просто великолепен! Но такими темпами это будет долго, давай я тебе помогу, добавив кнопки?";
+
+                button2.IsVisible = true;
+                button3.IsVisible = true;
+            }
+
+            label.Text = String.Format("Осталось кликнуть {0} раз.", maxNumberClick - clicksNumber);
+        }
+
+        private void clicked(object sender, EventArgs e) {
+            if (clicksNumber == maxNumberClick) {
+                infoLabel.Text = "Ты сделал это!";
+                return;
+            }
+
+            clicksNumber++;
+            updateStates();
         }
     }
 }
